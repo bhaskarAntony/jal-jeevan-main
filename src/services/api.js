14744236c0872911923019ba9836557f6eb99f27
bhaskarAntony <avinash80692@gmail.com>
2025-08-22 19:11:41 +0530

    all files

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_BASE_URL = 'http://localhost:3001/api'
-const API_BASE_URL = 'https://jal-jeevan.onrender.com/api'
+const API_BASE_URL = 'https://jal-jeevan-1.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
