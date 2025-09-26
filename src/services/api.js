7fc422792fdd9b8b7f57cda59b5d702e68001038
Abhiram Bharadwaj <abhirambharadwaj10@gmail.com>
2025-09-26 11:15:10 +0530

    Added New backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 // const API_BASE_URL = 'http://localhost:3001/api'
-const API_BASE_URL = 'https://jal-jeevan.onrender.com/api'
+//const API_BASE_URL = 'https://jal-jeevan.onrender.com/api'
+
+const API_BASE_URL = 'https://jal-jeevan-sctd.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
